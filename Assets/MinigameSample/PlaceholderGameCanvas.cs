f65fedcdd2d036034b5bf94274a92c76004e238b
forcepusher <bionitsoup@gmail.com>
2026-02-27 01:33:14 +0700

    Case sensitive changes

---
+++ Assets/MinigameSample/PlaceholderGameCanvas.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace BananaParty.Minigame.Sample
+{
+    public class PlaceholderGameCanvas : MonoBehaviour
+    {
+        public void OnStartGameButtonClick()
+        {
+
+        }
+
+        public void OnEndGameButtonClick()
+        {
+
+        }
+    }
+}
