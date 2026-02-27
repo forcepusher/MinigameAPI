15a0cd5f3bd46553178d50c886745ce6d3d97c27
forcepusher <bionitsoup@gmail.com>
2026-02-27 20:19:44 +0700

    Get things ready

--- Assets/MinigameSample/PlaceholderGameCanvas.cs
+++ Assets/MinigameSample/PlaceholderGameCanvas.cs
@@ -4,14 +4,37 @@ namespace BananaParty.Minigame.Sample
 {
     public class PlaceholderGameCanvas : MonoBehaviour
     {
+        private ClickerMinigame _clickerMinigame;
+
         public void OnStartGameButtonClick()
         {
-
+            _clickerMinigame = new ClickerMinigame();
+            _clickerMinigame.StartMinigame();
         }
 
         public void OnEndGameButtonClick()
         {
+            if (_clickerMinigame == null)
+                return;
 
+            Debug.Log("Minigame result = " + _clickerMinigame.MinigamePlayResult);
+
+            _clickerMinigame.EndMinigame();
+            _clickerMinigame = null;
+        }
+
+        public void Update()
+        {
+            if (_clickerMinigame == null)
+                return;
+
+            if (_clickerMinigame.IsMinigameFinished)
+            {
+                Debug.Log("Minigame result = " + _clickerMinigame.MinigamePlayResult);
+
+                _clickerMinigame.EndMinigame();
+                _clickerMinigame = null;
+            }
         }
     }
 }
