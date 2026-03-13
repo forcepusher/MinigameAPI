17566e5d46bda5cb17e88c421cfe6e9df52fe7bc
forcepusher <bionitsoup@gmail.com>
2026-03-13 18:23:56 +0700

    Add layers into an interface

--- Packages/com.bananaparty.minigame/Runtime/IMinigame.cs
+++ Packages/com.bananaparty.minigame/Runtime/IMinigame.cs
@@ -4,6 +4,16 @@ namespace BananaParty.Minigame
 {
     public interface IMinigame<TPlayResult>
     {
+        /// <summary>
+        /// Use this layer as your main layer to avoid conflicts with the main scene.
+        /// Light settings Culling Mask must be enabled only for this layer and exclusively.
+        /// </summary>
+        public int MainMinigameLayer { get => 30; }
+        /// <summary>
+        /// Use this layer for your own custom minigame logic that might need an extra layer.
+        /// </summary>
+        public int AdditionalMinigameLayer { get => 31; }
+
         public AsyncOperation StartMinigame(Camera mainSceneCamera);
 
         public AsyncOperation EndMinigame();
